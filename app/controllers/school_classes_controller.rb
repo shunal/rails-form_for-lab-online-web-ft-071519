483b3def44ec3dc62b4eb39d06333f8dfb168c9c
Steven Hunal <stevehunal@gmail.com>
2019-10-07 22:12:48 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,3 +1,19 @@
 class SchoolClassesController < ApplicationController
+  def index 
+  end 
   
+  def show 
+  end 
+  
+  def new 
+  end 
+  
+  def create
+  end 
+  
+  def edit
+  end
+  
+  def update
+  end 
 end 
