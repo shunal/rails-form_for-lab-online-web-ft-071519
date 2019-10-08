824bec3e537369e2b69482aa2c9ede1606af853a
Steven Hunal <stevehunal@gmail.com>
2019-10-08 00:50:58 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,4 +21,10 @@ class StudentsController < ApplicationController
   
   def update
   end 
+  
+  private
+  
+  def student_params(*args)
+    params.require(:student).permit(*args)
+  end 
 end 
