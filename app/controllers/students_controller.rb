1b18dfb2c24c1965930d59f1908f0d42fdcb136c
Steven Hunal <stevehunal@gmail.com>
2019-10-08 00:46:56 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,8 +9,13 @@ class StudentsController < ApplicationController
   def new 
     @student = Student.new
   end 
-  
+  @school_class = SchoolClass.new(school_class_params(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
   def create
+    @student = Student.new(student_params(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
   end 
   
   def edit
