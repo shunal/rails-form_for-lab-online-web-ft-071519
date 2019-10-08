f0b2acdd946c3b22c6e08a211f64ce2d549095ea
Steven Hunal <stevehunal@gmail.com>
2019-10-08 00:45:48 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -11,6 +11,9 @@ class SchoolClassesController < ApplicationController
   end 
   
   def create
+    @school_class = SchoolClass.new(school_class_params(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
   end 
   
   def edit
