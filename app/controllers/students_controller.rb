fbc64fe2b1935ef7e9e3b584ed10b680a5423f1c
Steven Hunal <stevehunal@gmail.com>
2019-10-08 00:55:28 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -17,9 +17,12 @@ class StudentsController < ApplicationController
   end 
   
   def edit
+    @student = Student.find(params[:id])
   end
   
   def update
+    @student = Student.find(params[:id])
+    @student.update(student_params(:first_name, :last_name))
   end 
   
   private
